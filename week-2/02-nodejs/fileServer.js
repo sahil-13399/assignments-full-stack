5427b7c270e2a49799f7f6baa075bc3e18f5672b
Sahil Samantaray <sahil13399@gmail.com>
2023-12-16 22:06:01 +0530

    Fixed failing test cases

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -12,10 +12,49 @@
     - For any other route not defined in the server return 404
     Testing the server - run `npm run test-fileServer` command in terminal
  */
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
-const app = express();
-
-
-module.exports = app;
+    const express = require('express');
+    const fs = require('fs');
+    const path = require('path');
+    const app = express();
+    
+    const filesDirectory = path.join(__dirname, 'files');
+    
+    app.get('/files', (req, res) => {
+      // Read the list of files in the ./files/ directory
+      fs.readdir(filesDirectory, (err, files) => {
+        if (err) {
+          res.status(500).json({ error: 'Internal Server Error' });
+        } else {
+          res.status(200).json(files);
+        }
+      });
+    });
+    
+    app.get('/file/:filename', (req, res) => {
+      const { filename } = req.params;
+      const filePath = path.join(filesDirectory, filename);
+    
+      // Read the content of the specified file
+      fs.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+          if (err.code === 'ENOENT') {
+            // File not found
+            res.status(404).send('File not found');
+          } else {
+            // Other error
+            res.status(500).json({ error: 'Internal Server Error' });
+          }
+        } else {
+          // File found, send the content
+          res.status(200).send(data);
+        }
+      });
+    });
+    
+    // Handle undefined routes
+    app.use((req, res) => {
+      res.status(404).send("Route not found");
+    });
+    
+    module.exports = app;
+    
